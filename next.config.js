e9e41e551ecc7f21f6497245126551816f3b9161
lilBunnyRabbit <andraz_ms@hotmail.com>
2023-04-12 18:48:17 +0200

    Initial code

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  distDir: "dist",
+  basePath: "/static-utils-v2",
+};
+
+module.exports = nextConfig;
