ca13d8e4a8713ca4e94a761ad58b62812772969a
lilBunnyRabbit <andraz_ms@hotmail.com>
2023-04-20 14:10:26 +0200

    Fixes deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
-const GH_REPO = "static-utils-v2";
-
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === "serve" ? "/" : `/${GH_REPO}/`,
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-  };
+  },
 });
