ed80cde43f4265c9d8ea28560dafe0f909260373
lilBunnyRabbit <andraz_ms@hotmail.com>
2023-04-20 14:39:02 +0200

    Fixes routing

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,21 +3,26 @@ import { ImageCropView } from "@/views/image-crop/ImageCropView";
 import { IndexView } from "@/views/index/IndexView";
 import { Navigate, createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      index: true,
+      element: <IndexView />,
+    },
+    {
+      path: "image-crop",
+      element: <ImageCropView />,
+    },
+    {
+      path: "image-concat",
+      element: <ImageConcatView />,
+    },
+    {
+      path: "*",
+      element: <Navigate to="/" />,
+    },
+  ],
   {
-    index: true,
-    element: <IndexView />,
-  },
-  {
-    path: "image-crop",
-    element: <ImageCropView />,
-  },
-  {
-    path: "image-concat",
-    element: <ImageConcatView />,
-  },
-  {
-    path: "*",
-    element: <Navigate to="/" />,
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
